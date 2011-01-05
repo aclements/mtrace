af59496e6807e59a9aa63bf001e178b6529bda2f
Silas Boyd-Wickizer <sbw@wether.csail.mit.edu>
2011-01-05 17:09:38 -0500

    [branch: mtrace]
    Oops, fix an off-by-one error in the backtrace code.

--- mtrace-tools/backtrace.py
+++ mtrace-tools/backtrace.py
@@ -28,13 +28,17 @@ class MtraceBacktracer:
         self.topId = None
         self.frames = None
 
-    def __walk_call_stack(self, currentId):
+    def __get_return_interval(self, topId):
         q = 'SELECT ret_id, end_pc FROM %s_call_intervals WHERE id = %lu'
         q = q % (self.dataName,
-                 currentId)
+                 topId)
+        return self.mtraceDB.exec_single(q)
 
-        r = self.mtraceDB.exec_single(q)
+    def __walk_call_stack(self, topId):
+        if topId == 0:
+            return
 
+        r = self.__get_return_interval(topId)
         retId = r[0]
         pc = r[1]
 
@@ -53,6 +57,7 @@ class MtraceBacktracer:
         pc = r[1]
         
         self.frames = []
+        # Save the top frame
         self.frames.append(MtraceCallInterval(pc))
 
         q = 'SELECT id FROM %s_call_intervals WHERE cpu = %u AND ' + \
@@ -63,7 +68,10 @@ class MtraceBacktracer:
                  self.accessId)
 
         topId = self.mtraceDB.exec_single(q)[0]
-        self.__walk_call_stack(topId)
+        # Skip the top interval, which we already accounted for
+        nextId = self.__get_return_interval(topId)[0]
+
+        self.__walk_call_stack(nextId)
 
     def get_depth(self):
         if self.frames == None:
@@ -87,7 +95,7 @@ class MtraceBacktracer:
 
         def next(self):
             if self.bt.get_depth() == self.i:
-                raise StopIteration();
+                raise StopIteration()
             interval = self.bt.get_interval(self.i)
             self.i += 1
             return interval
