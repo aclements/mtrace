8df69762fe9cd9d08b66135fd98dfa388a91baa6
Nickolai Zeldovich <nickolai@csail.mit.edu>
2011-05-18 10:39:03 -0700

    compile fix

--- mtrace-tools/addr2line.cc
+++ mtrace-tools/addr2line.cc
@@ -76,7 +76,9 @@ Addr2line::lookup(uint64_t pc, char **func, char **file, int *line)
 {
 	char buf[4096];
 	int n = snprintf(buf, sizeof(buf), "%#"PRIx64"\n", pc);
-	write(_out, buf, n);
+	if (n != write(_out, buf, n))
+		edie("%s: write", __func__);
+
 	n = 0;
 	while (1) {
 		int r = read(_in, buf + n, sizeof(buf) - n - 1);
