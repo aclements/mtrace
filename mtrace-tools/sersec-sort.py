df542fab7d41da40508c01b1825050275ddc755b
Nickolai Zeldovich <nickolai@csail.mit.edu>
2011-06-02 14:44:35 -0400

    sort the per-pc coherence miss lists

--- mtrace-tools/sersec-sort.py
+++ mtrace-tools/sersec-sort.py
@@ -7,6 +7,7 @@ serials = json.loads(sys.stdin.read())["serial-sections"]
 serials = sorted(serials, key=lambda sec: sec["total-cycles"], reverse=True)
 for s in serials:
     s["per-acquire-pc"] = sorted(s["per-acquire-pc"], key=lambda sec: sec["total-cycles"], reverse=True)
-for s in serials:
     s["coherence-miss-list"] = sorted(s["coherence-miss-list"], key=lambda sec: sec["count"], reverse=True)
+    for p in s["per-acquire-pc"]:
+        p["coherence-miss-list"] = sorted(p["coherence-miss-list"], key=lambda sec: sec["count"], reverse=True)
 print json.dumps(serials, indent=4)
