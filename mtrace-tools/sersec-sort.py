e464e2891f309f773295871c3f29f06234aa1ccb
Nickolai Zeldovich <nickolai@csail.mit.edu>
2011-06-02 21:42:57 -0400

    move up contended sections

--- mtrace-tools/sersec-sort.py
+++ mtrace-tools/sersec-sort.py
@@ -3,11 +3,18 @@
 import sys
 import json
 
+def secsort(sec):
+    return sec["total-cycles"]*min(sec["per-cpu-percent"])
+
 serials = json.loads(sys.stdin.read())["serial-sections"]
-serials = sorted(serials, key=lambda sec: sec["total-cycles"], reverse=True)
+serials = sorted(serials, key=secsort, reverse=True)
 for s in serials:
-    s["per-acquire-pc"] = sorted(s["per-acquire-pc"], key=lambda sec: sec["total-cycles"], reverse=True)
-    s["coherence-miss-list"] = sorted(s["coherence-miss-list"], key=lambda sec: sec["count"], reverse=True)
+    s["per-acquire-pc"] = sorted(s["per-acquire-pc"], key=secsort, reverse=True)
+    s["coherence-miss-list"] = sorted(s["coherence-miss-list"],
+				      key=lambda sec: sec["count"],
+				      reverse=True)
     for p in s["per-acquire-pc"]:
-        p["coherence-miss-list"] = sorted(p["coherence-miss-list"], key=lambda sec: sec["count"], reverse=True)
+        p["coherence-miss-list"] = sorted(p["coherence-miss-list"],
+					  key=lambda sec: sec["count"],
+					  reverse=True)
 print json.dumps(serials, indent=4)
