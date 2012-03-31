e93612d42b5f582b729f6b807bc192a060468a53
Austin Clements <amdragon@mit.edu>
2012-03-30 21:11:17 -0400

    Teach mtrace-magic.c about different types of recording

--- mtrace-tools/mtrace-magic.c
+++ mtrace-tools/mtrace-magic.c
@@ -3,13 +3,15 @@
 
 int main(int ac, char **av)
 {
-    if (ac != 3)
-        die("usage: %s enable|disable name", basename(av[0]));
-
-    if (av[1][0] == 'e')
+    if (ac == 3 && strcmp(av[1], "movement") == 0)
         mtrace_enable_set(mtrace_record_movement, av[2]);
-    else
+    else if (ac == 3 && strcmp(av[1], "ascope") == 0)
+        mtrace_enable_set(mtrace_record_ascope, av[2]);
+    else if (ac == 3 && strcmp(av[1], "disable") == 0)
         mtrace_enable_set(mtrace_record_disable, av[2]);
-
+    else {
+        fprintf(stderr, "usage: %s movement|ascope|disable name", basename(av[0]));
+        return 2;
+    }
     return 0;
 }
