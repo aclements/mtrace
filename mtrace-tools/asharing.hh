5f61fd0cbd41cb4487097d346881e0ce26f6ff94
Austin Clements <amdragon@mit.edu>
2012-05-04 17:25:24 -0400

    Hack to report stuff shared between shared scope pairs

--- mtrace-tools/asharing.hh
+++ mtrace-tools/asharing.hh
@@ -103,6 +103,81 @@ public:
 
         summary->done();
 
+        // XXX Complete hack to see what's shared between logically
+        // shared pairs
+        if (0) {
+            JsonList *lst = JsonList::create();
+            json_file->put("sharing", lst, false);
+
+            for (auto it1 = scopes_.begin(); it1 != scopes_.end(); ++it1) {
+                const Ascope &s1 = *it1;
+                for (auto it2 = it1+1; it2 != scopes_.end(); ++it2) {
+                    const Ascope &s2 = *it2;
+                    // If the two scopes ran on the same CPU, we'll
+                    // get lots of "sharing" on per-CPU data, so don't
+                    // compare scopes from the same CPU
+                    if (s1.cpu_set_ == s2.cpu_set_)
+                        continue;
+
+                    auto abstract_sharing =
+                        shares(s1.aread_.begin(),  s1.aread_.end(),
+                               s1.awrite_.begin(), s1.awrite_.end(),
+
+                               s2.aread_.begin(),  s2.aread_.end(),
+                               s2.awrite_.begin(), s2.awrite_.end());
+                    auto concrete_sharing =
+                        shares(s1.read_.begin(),  s1.read_.end(),
+                               s1.write_.begin(), s1.write_.end(),
+
+                               s2.read_.begin(),  s2.read_.end(),
+                               s2.write_.begin(), s2.write_.end());
+
+                    if (abstract_sharing && concrete_sharing) {
+                        JsonDict *od = JsonDict::create();
+                        od->put("s1", s1.name_);
+                        od->put("s2", s2.name_);
+                        auto bytes = 
+                            shared_amount(s1.read_.begin(),  s1.read_.end(),
+                                          s1.write_.begin(), s1.write_.end(),
+
+                                          s2.read_.begin(),  s2.read_.end(),
+                                          s2.write_.begin(), s2.write_.end());
+                        od->put("rw", bytes.first);
+                        od->put("ww", bytes.second);
+
+                        JsonList *abstract = JsonList::create();
+                        int count = 0;
+                        shared_avars_to_json(abstract, &count,
+                                             s1.aread_.begin(),  s1.aread_.end(),
+                                             s2.awrite_.begin(), s2.awrite_.end());
+                        shared_avars_to_json(abstract, &count,
+                                             s1.awrite_.begin(), s1.awrite_.end(),
+                                             s2.aread_.begin(),  s2.aread_.end());
+                        shared_avars_to_json(abstract, &count,
+                                             s1.awrite_.begin(), s1.awrite_.end(),
+                                             s2.awrite_.begin(), s2.awrite_.end());
+                        od->put("abstract", abstract);
+
+                        // JsonList *phys = JsonList::create();
+                        // count = 0;
+                        // shared_to_json(phys, &count,
+                        //                s1.read_.begin(),  s1.read_.end(),
+                        //                s2.write_.begin(), s2.write_.end());
+                        // shared_to_json(phys, &count,
+                        //                s1.write_.begin(), s1.write_.end(),
+                        //                s2.read_.begin(),  s2.read_.end());
+                        // shared_to_json(phys, &count,
+                        //                s1.write_.begin(), s1.write_.end(),
+                        //                s2.write_.begin(), s2.write_.end());
+                        // od->put("physical", phys);
+
+                        lst->append(od);
+                    }
+                }
+            }
+            lst->done();
+        }
+
         if (ascopes_) {
             // Raw abstract and concrete sets
             JsonList *lst = JsonList::create();
@@ -113,6 +188,7 @@ public:
                 od->put("aread", JsonList::create(ascope.aread_.begin(), ascope.aread_.end()));
                 od->put("awrite", JsonList::create(ascope.awrite_.begin(), ascope.awrite_.end()));
 
+#if 0
                 JsonList *rw;
                 rw = JsonList::create();
                 for (auto &it : ascope.read_)
@@ -122,6 +198,7 @@ public:
                 for (auto &it : ascope.write_)
                     rw->append(it.second.to_json());
                 od->put("write", rw);
+#endif
 
                 lst->append(od);
             }
@@ -386,5 +463,57 @@ private:
                 first2++;
             }
         }
+    }
+
+    template<class InputIterator1, class InputIterator2>
+    void shared_avars_to_json(JsonList *shared, int *count,
+                              InputIterator1 first1, InputIterator1 last1,
+                              InputIterator2 first2, InputIterator2 last2)
+    {
+        while (first1 != last1 && first2 != last2) {
+            if (*first1 < *first2)
+                ++first1;
+            else if (*first2 < *first1)
+                ++first2;
+            else {
+                if (*count < 25)
+                    shared->append(*first1);
+                ++*count;
+                first1++;
+                first2++;
+            }
+        }
+    }
+
+    template<class InputIterator1, class InputIterator2>
+    pair<uint64_t,uint64_t>
+    shared_amount(InputIterator1 r1begin, InputIterator1 r1end,
+                  InputIterator1 w1begin, InputIterator1 w1end,
+                  InputIterator2 r2begin, InputIterator2 r2end,
+                  InputIterator2 w2begin, InputIterator2 w2end)
+    {
+        return
+            make_pair(shared_amount_1(r1begin, r1end, w2begin, w2end) +
+                      shared_amount_1(w1begin, w1end, r2begin, r2end),
+                      shared_amount_1(w1begin, w1end, w2begin, w2end));
+    }
+
+    template<class InputIterator1, class InputIterator2>
+    uint64_t shared_amount_1(InputIterator1 first1, InputIterator1 last1,
+                             InputIterator2 first2, InputIterator2 last2)
+    {
+        uint64_t out = 0;
+        while (first1 != last1 && first2 != last2) {
+            if (*first1 < *first2)
+                ++first1;
+            else if (*first2 < *first1)
+                ++first2;
+            else {
+                out += std::min(first1->second.size, first2->second.size);
+                first1++;
+                first2++;
+            }
+        }
+        return out;
     }
 };
